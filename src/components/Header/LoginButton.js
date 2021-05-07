163036ee49aac8f7bf630212b534c6c47d07a58a
Todd Albert <todd@bocacode.com>
2021-05-06 23:31:04 -0400

    fix firebase

--- src/components/Header/LoginButton.js
+++ src/components/Header/LoginButton.js
@@ -1,14 +1,16 @@
 import React, { useState } from 'react'
 import firebase from 'firebase'
 
-firebase.initializeApp({
-  apiKey: "AIzaSyC9lr-QBQGv6Jqp61tuuvD_VvZ4rfjYq-4",
-  authDomain: "affirm-bc.firebaseapp.com",
-  projectId: "affirm-bc",
-  storageBucket: "affirm-bc.appspot.com",
-  messagingSenderId: "978102774864",
-  appId: "1:978102774864:web:f6d1313263276c17317555"
-})
+if (firebase.apps.length === 0) {
+  firebase.initializeApp({
+    apiKey: "AIzaSyC9lr-QBQGv6Jqp61tuuvD_VvZ4rfjYq-4",
+    authDomain: "affirm-bc.firebaseapp.com",
+    projectId: "affirm-bc",
+    storageBucket: "affirm-bc.appspot.com",
+    messagingSenderId: "978102774864",
+    appId: "1:978102774864:web:f6d1313263276c17317555"
+  })
+}
 
 function LoginButton() {
   const [user, setUser] = useState(null)
@@ -27,9 +29,7 @@ function LoginButton() {
       backgroundImage: `url("${user.photoURL}")`,
       backgroundSize: "cover"
     }
-    : {
-
-    }
+    : { }
   return (
     <button
       style={style}
